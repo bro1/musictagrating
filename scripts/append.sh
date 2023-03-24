a8f133ae43db20a085a59f865e2f15f01f469355
Linas Jakucionis <linasj@gmail.com>
2023-03-24 21:37:12 +1300

    added documentation

--- scripts/append.sh
+++ scripts/append.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Open an input file
+# input file is a tab delimited file containing the following columns
+# 1. file name e.g. this is a file.mp3
+# 2. star rating 1-5 e.g. 3
+# 3. /home/someone/syncthing-data/rate/album/this is a file.mp3
+#
+# The script will output the following columns:
+# 1.    artist - song name (as extracted by mp3info)
+# 2-4.  all columns from original input file
+# 5. URL to search for that song on Youtube Music
+# 6. URL to search for that song on Spotify music
+
+
 input_file=$1
 
 while read line; do
